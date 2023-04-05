1f153ada3732751badbdaa8a62cfa2214e7a049b
MartinaPerozzi <perozzimartina@gmail.com>
2023-04-05 20:04:10 +0200

    TO DO: add Faker

---
+++ database/migrations/2023_04_03_180824_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 60);
+            $table->string('stazione_partenza', 60);
+            $table->string('stazione_arrivo', 60);
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno')->unique();
+            $table->tinyInteger('numero_carrozze')->nullable();
+            $table->boolean('in_orario')->nullable();
+            $table->boolean('cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
---
+++ resources/views/trains/list.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('card_content')
+    <div class="row row-cols-4">
+        @forelse ($trains as $train)
+            <div class="col">
+                <div class="card mb-4" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $train->azienda }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{{ $train->stazione_partenza }}</h6>
+                        <span class="card-text">{{ $train->stazione_arrivo }}</span>
+                        <span>{{ $train->orario_partenza }}</span>
+                        <span>{{ $train->orario_arrivo }}</span>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <h3>No movies found</h3>
+        @endforelse
+    </div>
+@endsection
